52718685e9e41289de2572e1515fe31abeee4d87
sylvesterd <sylvesterdung2@gmail.com>
2024-11-07 17:09:41 +0100

    First work release of task manager

---
+++ main.py
@@ -0,0 +1,84 @@
+# Imports
+from flask import Flask, render_template, redirect, request
+from flask_scss import Scss
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+Scss(app)
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+# When deploying
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+# initialize the app with the extension
+db = SQLAlchemy(app)
+
+class myTasks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable = False)
+    complete = db.Column(db.Integer, default=0)
+    created = db.Column(db.DateTime, default=datetime.now())
+    
+    def __repr__(self) -> str:
+        return f"Task {self.id}"
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    # Add task
+    if request.method == "POST":
+        current_task = request.form['content']
+        new_task = myTasks(content=current_task)
+        
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            print(f"ERROR: {e}")
+            return f"ERROR: {e}"
+    else:
+        # See current tasks
+        tasks = myTasks.query.order_by(myTasks.created).all()
+        return render_template("index.html", tasks = tasks)
+
+    
+# Delete an Item
+@app.route("/delete/<int:id>")
+def delete(id: int):
+    delete_task = myTasks.query.get_or_404(id)
+    
+    try:
+        db.session.delete(delete_task)
+        db.session.commit()
+        return redirect("/")
+    except Exception as e:
+        print(f"ERROR: {e}")
+    
+   
+# Edit an Item
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit(id: int):
+    task = myTasks.query.get_or_404(id)
+    
+    if request.method == "POST":
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            return f"ERROR: {e}"
+    else:
+        return render_template("edit.html", task = task)
+
+
+
+# Runner and Debugger
+if __name__ == "__main__":
+    # with app.app_context():
+       # db.create_all()
+        
+    app.run(debug=True)
